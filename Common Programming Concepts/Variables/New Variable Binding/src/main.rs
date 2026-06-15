860740a9ad2aa8cacd8049f3a906b78548ab51ea
datatalking <paintstone@gmail.com>
2026-06-14 20:38:05 -0700

    Initial commit lesson 3

--- Common Programming Concepts/Variables/New Variable Binding/src/main.rs
+++ Common Programming Concepts/Variables/New Variable Binding/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     //TODO: x = 5;
-    println!("x has the value {}", x);
+    println!("x has the value {}", 5);
 }
--- Introduction/Getting started/Editor/src/main.rs
+++ Introduction/Getting started/Editor/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let origional_price = 51:
+    println!("Your sale price is {}"sale_price(original_price)):
     // put your code here to launch it
 }
+
+fn sale_price(price: 132) ->{
+    if_event(price) {
+        price - 10
+    } else {
+        price - 3
+    }
+}
+
+fn is_even(num: i32) -> bool {num % 2 == 0}
